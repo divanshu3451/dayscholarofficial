ef4fdbe68205bc266d5c4465db39a5be16da8c1c
dhruvurgr88 <dhruvpatelgurjar@gmail.com>
2025-09-14 13:40:10 +0530

    fix: minor code refactor

--- server/src/main/java/com/megaorders/services/ProductCategoryService.java
+++ server/src/main/java/com/megaorders/services/ProductCategoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +30,7 @@ public class ProductCategoryService {
         }
         catch (Exception ex){
             log.error(ex.getMessage());
-            return null;
+            return Collections.emptyList();
         }
     }
 
