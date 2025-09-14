909dca993469eb5d6fabe5e25fd3f01ac26a5197
dhruvurgr88 <dhruvpatelgurjar@gmail.com>
2025-09-14 13:25:33 +0530

    Fetch all categories

---
+++ server/src/main/java/com/megaorders/dtos/ProductCategoryResponseDTO.java
@@ -0,0 +1,17 @@
+package com.megaorders.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategoryResponseDTO {
+
+    private String name;
+    private String description;
+
+}
